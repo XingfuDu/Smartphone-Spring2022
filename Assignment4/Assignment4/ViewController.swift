8f7d5a55a1b014216bd43dfbcdfa9c010a3a676d
duxingfu <du.xingf@northeastern.edu>
2022-02-23 23:11:09 -0800

    add assignment 4

--- Assignment4/Assignment4/ViewController.swift
+++ Assignment4/Assignment4/ViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, SendFirstAndLastNameDelegate {
+    
+    @IBOutlet weak var lblFirstName: UILabel!
+    @IBOutlet weak var lblLastName: UILabel!
+    @IBOutlet weak var lblWelcome: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getName(_ sender: Any) {
+        performSegue(withIdentifier: "segueToSVC", sender: self)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "segueToSVC" {
+            let SVC = segue.destination as! SecondViewController
+            guard let firstName = lblFirstName.text else {return}
+            guard let lastName = lblLastName.text else {return}
+            SVC.firstName = firstName
+            SVC.lastName = lastName
+            SVC.sendFirstAndLastNameDelegate = self
+        }
+    }
+    func setFirstAndLastName(firstName: String, lastName: String) {
+        lblFirstName.text = firstName
+        lblLastName.text = lastName
+    }
+    func setWelcomeText(welcomeText: String) {
+        lblWelcome.text = welcomeText
+    }
 }
 
